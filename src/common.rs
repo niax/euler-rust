3dca06eab07d0a93f10073a248db3780a70896e6
Nicholas Hollett <niax@niax.co.uk>
2014-09-29 14:46:19 +0100

    Move PrimeIterator into common.
    Improves implementation to use a growable bit vector to store a table of
    prime/not prime.

--- src/common.rs
+++ src/common.rs
@@ -1,3 +1,6 @@
+use std::iter::range_step;
+use std::collections::Bitv;
+
 pub struct FactorIterator {
     value: uint,
     current: uint,
@@ -62,9 +65,75 @@ pub fn lcm(a: uint, b: uint) -> uint {
     }
 }
 
+/// Iterator over prime numbers from 2
+pub struct PrimeIterator {
+    current: uint,
+    bitv: Bitv,
+}
+
+
+impl PrimeIterator {
+    pub fn new() -> PrimeIterator {
+        let mut initial_vector = Bitv::with_capacity(8, false);
+        // Preset the initial vector with primes
+        for i in vec![2, 3, 5, 7].iter() {
+            initial_vector.set(*i, true);
+        }
+        PrimeIterator {
+            current: 0,
+            bitv: initial_vector,
+        }
+    }
+
+    // Double the size of the bitv
+    fn grow_table(&mut self) {
+        let first_number = self.bitv.len();
+        // Default to true, we'll set things to false as we go
+        // what's left are primes
+        self.bitv.grow(first_number, true);
+        let new_length = self.bitv.len();
+        for i in range(1, new_length) {
+            // If prime, set multiples to false
+            if self.bitv.get(i) {
+                // Find the first multiple of this prime in the newly created
+                // table space
+                let first_multiple = if i < first_number {
+                    if first_number % i == 0 {
+                        first_number
+                    } else {
+                        (first_number - first_number % i) + i
+                    }
+                } else {
+                    i * 2
+                };
+                for j in range_step(first_multiple, new_length, i) {
+                    self.bitv.set(j, false);
+                }
+            }
+        }
+    }
+}
+
+impl Iterator<uint> for PrimeIterator {
+    fn next(&mut self) -> Option<uint> {
+        loop {
+            // Consider the next number
+            self.current += 1;
+            // If we've gone over the bounds of our Bitv, grow it
+            if self.bitv.len() <= self.current {
+                self.grow_table();
+            }
+
+            if self.bitv.get(self.current) {
+                return Some(self.current);
+            }
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::{FactorIterator, is_prime, lcm, gcd};
+    use super::{FactorIterator, is_prime, lcm, gcd, PrimeIterator};
 
     #[test]
     fn test_factorization() {
@@ -90,5 +159,33 @@ mod test {
     fn test_greatest_common_divisor() {
         assert_eq!(gcd(5, 5), 5);
         assert_eq!(gcd(54, 24), 6);
+    }
+
+    #[test]
+    fn test_prime_iterator() {
+        let mut iter = PrimeIterator::new();
+        let primes = vec![
+            2u, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+            31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
+            73, 79, 83, 89, 97, 101, 103, 107, 109, 113,
+            127, 131, 137, 139, 149, 151, 157, 163, 167, 173,
+            179, 181, 191, 193, 197, 199, 211, 223, 227, 229,
+            233, 239, 241, 251, 257, 263, 269, 271, 277, 281,
+            283, 293, 307, 311, 313, 317, 331, 337, 347, 349,
+            353, 359, 367, 373, 379, 383, 389, 397, 401, 409,
+            419, 421, 431, 433, 439, 443, 449, 457, 461, 463,
+            467, 479, 487, 491, 499, 503, 509, 521, 523, 541,
+            547, 557, 563, 569, 571, 577, 587, 593, 599, 601,
+            607, 613, 617, 619, 631, 641, 643, 647, 653, 659,
+            661, 673, 677, 683, 691, 701, 709, 719, 727, 733,
+            739, 743, 751, 757, 761, 769, 773, 787, 797, 809,
+            811, 821, 823, 827, 829, 839, 853, 857, 859, 863,
+            877, 881, 883, 887, 907, 911, 919, 929, 937, 941,
+            947, 953, 967, 971, 977, 983, 991, 997, 1009, 1013
+        ];
+        for i in primes.iter() {
+            println!("Looking for {}", *i);
+            assert_eq!(iter.next().unwrap(), *i);
+        }
     }
 }
--- src/prob7.rs
+++ src/prob7.rs
@@ -1,45 +1,4 @@
-struct PrimeIterator {
-    previous: Vec<uint>,
-    current: uint,
-}
-
-
-impl PrimeIterator {
-    pub fn new() -> PrimeIterator {
-        PrimeIterator {
-            previous: Vec::new(),
-            current: 1
-        }
-    }
-}
-
-impl Iterator<uint> for PrimeIterator {
-    fn next(&mut self) -> Option<uint> {
-        if self.current == 1 {
-            self.current = 2;
-            self.previous.push(2);
-            return Some(2);
-        }
-
-        loop {
-            self.current += 1;
-            let mut good = true;
-            // limit division testing by the largest possible factor
-            let max_factor = (self.current as f64).sqrt().ceil() as uint;
-            for &i in self.previous.iter() {
-                if self.current % i == 0 && i <= max_factor {
-                    good = false;
-                    break;
-                }
-            }
-
-            if good {
-                self.previous.push(self.current);
-                return Some(self.current);
-            }
-        }
-    }
-}
+use common::PrimeIterator;
 
 /// Returns the nth prime.
 fn nth_prime(n: uint) -> uint {
@@ -49,18 +8,8 @@ fn nth_prime(n: uint) -> uint {
 
 #[cfg(test)]
 mod test {
-    use super::{nth_prime, PrimeIterator};
+    use super::nth_prime;
 
-    #[test]
-    fn test_prime_iterator() {
-        let mut iter = PrimeIterator::new();
-        assert_eq!(iter.next().unwrap(), 2u);
-        assert_eq!(iter.next().unwrap(), 3u);
-        assert_eq!(iter.next().unwrap(), 5u);
-        assert_eq!(iter.next().unwrap(), 7u);
-        assert_eq!(iter.next().unwrap(), 11u);
-        assert_eq!(iter.next().unwrap(), 13u);
-    }
 
     #[test]
     fn provided_example() {
